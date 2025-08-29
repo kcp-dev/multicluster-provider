f58a9b45ae4239205e7adb5a4c7074078695d529
Marvin Beckers <marvin@kubermatic.com>
2025-08-29 11:21:54 +0200

    Wire forked event recorder into apiexport provider
    On-behalf-of: SAP <marvin.beckers@sap.com>
    Signed-off-by: Marvin Beckers <marvin@kubermatic.com>

--- examples/apiexport/main.go
+++ examples/apiexport/main.go
@@ -114,6 +114,8 @@ func main() {
 				}
 
 				log.Info("Reconciling ConfigMap", "name", s.Name, "uuid", s.UID)
+				recorder := cl.GetEventRecorderFor("kdp-configmap-controller")
+				recorder.Eventf(s, corev1.EventTypeNormal, "ConfigMap Reconciled", "ConfigMap %s reconciled", s.Name)
 
 				return reconcile.Result{}, nil
 			},
