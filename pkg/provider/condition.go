2a1557869dc96e40831601b6e9427315760ecd89
Nelo-T. Wallus <n.wallus@sap.com>
2026-05-11 08:57:41 +0200

    Add pkg/provider.URLExtractor interface and .ExtractURLsByJSONPath
    Signed-off-by: Nelo-T. Wallus <red.brush9525@fastmail.com>
    Signed-off-by: Nelo-T. Wallus <n.wallus@sap.com>

--- pkg/provider/condition.go
+++ pkg/provider/condition.go
@@ -25,39 +25,6 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-// DefaultExtractURLsFromEndpointSlice extracts virtual workspace URLs from
-// any object with a .status.endpoints[].url structure (e.g. APIExportEndpointSlice).
-func DefaultExtractURLsFromEndpointSlice(obj client.Object) ([]string, error) {
-	unstructuredMap, err := runtime.DefaultUnstructuredConverter.ToUnstructured(obj)
-	if err != nil {
-		return nil, fmt.Errorf("unable to convert object to unstructured map: %w", err)
-	}
-
-	endpoints, found, err := unstructured.NestedSlice(unstructuredMap, "status", "endpoints")
-	if err != nil {
-		return nil, fmt.Errorf("error getting status.endpoints from unstructured map: %w", err)
-	}
-	if !found {
-		return nil, nil
-	}
-
-	var urls []string
-	for _, epRaw := range endpoints {
-		ep, ok := epRaw.(map[string]any)
-		if !ok {
-			continue
-		}
-		url, _, err := unstructured.NestedString(ep, "url")
-		if err != nil {
-			return nil, fmt.Errorf("error getting url from endpoint %v: %w", ep, err)
-		}
-		if url != "" {
-			urls = append(urls, url)
-		}
-	}
-	return urls, nil
-}
-
 // ConditionReadyFunc returns a function that checks if the given object has a condition with the given type and status True.
 func ConditionReadyFunc(conditionType string) func(client.Object) (bool, error) {
 	return func(obj client.Object) (bool, error) {
